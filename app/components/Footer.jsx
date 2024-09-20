2b0b7f8a5e5d0d13fb22761d0822ca49d61e36b1
Prince-Sinha <211114058@stu.manit.ac.in>
2024-09-20 08:16:13 +0530

    fix(client): fix login button

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-    return <div className="h-32 w-full md:w-1/3 flex flex-col justify-center border rounded-lg border-slate-300 bg-gray-200">
+    return <div className="h-28 w-full md:w-1/3 flex flex-col justify-center border rounded-lg border-slate-300 bg-gray-200">
         <div className="p-5 flex">
-            <div className="pl-5 pr-5 mb-3 font-bold text-lg">DOWNLOAD APP
+            <div className="pl-5 pr-5 mb-3 text-sm">DOWNLOAD APP
             <div className="grid grid-cols-6">
                 <div className="text-sm pl-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="0.82em" height="1em" viewBox="0 0 256 315"><path d="M213.803 167.03c.442 47.58 41.74 63.413 42.197 63.615c-.35 1.116-6.599 22.563-21.757 44.716c-13.104 19.153-26.705 38.235-48.13 38.63c-21.05.388-27.82-12.483-51.888-12.483c-24.061 0-31.582 12.088-51.51 12.871c-20.68.783-36.428-20.71-49.64-39.793c-27-39.033-47.633-110.3-19.928-158.406c13.763-23.89 38.36-39.017 65.056-39.405c20.307-.387 39.475 13.662 51.889 13.662c12.406 0 35.699-16.895 60.186-14.414c10.25.427 39.026 4.14 57.503 31.186c-1.49.923-34.335 20.044-33.978 59.822M174.24 50.199c10.98-13.29 18.369-31.79 16.353-50.199c-15.826.636-34.962 10.546-46.314 23.828c-10.173 11.763-19.082 30.589-16.678 48.633c17.64 1.365 35.66-8.964 46.64-22.262"/></svg>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,18 +6,18 @@ export default function Header(){
         <>
         <div className="header">
             <div>
-                <img src="/indmoney.png" alt="" className="w-1/2" />
+                <img src="/indmoney.png" alt="" className="w-1/3" />
             </div>
             <div className="">
                 <ul className="options">
-                    <li className="px-5 py-2 border border-[#39a451] text-[#39a451] rounded-full font-semibold ">Log In</li>
-                    <li className="items-center">
+                    <li className="pl-4 pr-4 pt-1 pb-1 border border-[#39a451] rounded-full text-[#39a451] text-md">Log&nbsp;In</li>
+                    <li className="self-center">
                     <img src="/customer-care (1).png" alt="" className="" />
                     </li>
-                    <li className="items-center">
+                    <li className="self-center">
                     <img src="/search.png" alt="" className="w-3/4 item-center" />
                     </li>
-                    <li className="align-middle items-center">
+                    <li className="align-middle items-center self-center">
                     <img src="/close.png" alt="" className="w-3/4 item-center" />
                     </li>
                 </ul>
